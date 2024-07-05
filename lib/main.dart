d0a98e46d175de07473ca088aaa91f75038615cc
liangjiang <jiangliang.net@gmail.com>
2024-07-05 09:07:13 +0800

    添加sentry_flutter apk_installer permission_handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'utils/awesome_notifications_helper.dart';
 
 import 'app/data/local/my_hive.dart';
@@ -30,38 +35,68 @@ Future<void> main() async {
   // initialize local notifications service
   await AwesomeNotificationsHelper.init();
 
-  runApp(
-    ScreenUtilInit(
-      // todo add your (Xd / Figma) artboard size
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      useInheritedMediaQuery: true,
-      rebuildFactor: (old, data) => true,
-      builder: (context, widget) {
-        return GetMaterialApp(
-          // todo add your app name
-          title: "我的flutter模板",
-          useInheritedMediaQuery: true,
-          debugShowCheckedModeBanner: false,
-          builder: (context,widget) {
-            bool themeIsLight = MySharedPref.getThemeIsLight();
-            return Theme(
-              data: MyTheme.getThemeData(isLight: themeIsLight),
-              child: MediaQuery(
-                // prevent font from scalling (some people use big/small device fonts)
-                // but we want our app font to still the same and dont get affected
-                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-                child: widget!,
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    ///注册异常上报平台
+    SentryFlutter.init(
+            (options) {
+          options.autoAppStart = false;
+          // options.beforeSend = (SentryEvent event, Hint hint) async {
+          //   final bytes = await WebViewFactory.instance.createWebView()
+          //       .captureScreenshot();
+          //   if (bytes != null) {
+          //     hint.screenshot = SentryAttachment.fromScreenshotData(bytes);
+          //   }
+          //   return event;
+          // };
+          options.dsn =
+          'https://d8647af36674b068c016d9b2bf9a8a05@o4507146122887168.ingest.us.sentry.io/4507146124656640';
+          // To set a uniform sample rate
+          options.tracesSampleRate = 1.0;
+        },
+        appRunner: () =>
+            runApp(
+              ScreenUtilInit(
+                designSize: const Size(375, 812),
+                minTextAdapt: true,
+                splitScreenMode: true,
+                useInheritedMediaQuery: true,
+                rebuildFactor: (old, data) => true,
+                builder: (context, widget) {
+                  return GetMaterialApp(
+                    // todo add your app name
+                    title: "我的flutter模板",
+                    useInheritedMediaQuery: true,
+                    debugShowCheckedModeBanner: false,
+                    builder: (context, widget) {
+                      bool themeIsLight = MySharedPref.getThemeIsLight();
+                      return Theme(
+                        data: MyTheme.getThemeData(isLight: themeIsLight),
+                        child: MediaQuery(
+                          // prevent font from scalling (some people use big/small device fonts)
+                          // but we want our app font to still the same and dont get affected
+                          data: MediaQuery.of(context).copyWith(
+                              textScaleFactor: 1.0),
+                          child: widget!,
+                        ),
+                      );
+                    },
+                    initialRoute: AppPages.INITIAL,
+                    // first screen to show when app is running
+                    getPages: AppPages.routes,
+                    // app screens
+                    locale: MySharedPref.getCurrentLocal(),
+                    // app language
+                    translations: LocalizationService
+                        .getInstance(), // localization services in app (controller app language)
+                  );
+                },
               ),
-            );
-          },
-          initialRoute: AppPages.INITIAL, // first screen to show when app is running
-          getPages: AppPages.routes, // app screens
-          locale: MySharedPref.getCurrentLocal(), // app language
-          translations: LocalizationService.getInstance(), // localization services in app (controller app language)
-        );
-      },
-    ),
-  );
+            )
+    );
+  }, (dynamic error, StackTrace stackTrace) async {
+    Logger().e("$error\r\n$stackTrace");
+    await Sentry.captureException(error, stackTrace: stackTrace);
+  });
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <awesome_notifications/awesome_notifications_plugin_c_api.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <sentry_flutter/sentry_flutter_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AwesomeNotificationsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AwesomeNotificationsPluginCApi"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  SentryFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
 }
